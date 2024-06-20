9b18adb2fc19ab553b6955598801d5248467befb
Jaykumar26 <131598702+Jaykumar26@users.noreply.github.com>
2024-06-20 12:06:27 +0530

    Added my project

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    
+    public static void main(String args[]){
+        System.out.println("This is Java Program");
+        System.out.println("Output from Java Program");
+        System.out.println("HelloWorld");
+        
+    }
+}
