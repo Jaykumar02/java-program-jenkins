8a73af7eef928bfe61f388069a475855da47bb7d
Jaykumar26 <131598702+Jaykumar26@users.noreply.github.com>
2024-06-20 12:45:33 +0530

    Added my project

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String args[]){
         System.out.println("This is Java Program");
         System.out.println("Output from Java Program");
-        System.out.println("HelloWorld");
+        System.out.println("Hello");
         
     }
 }
